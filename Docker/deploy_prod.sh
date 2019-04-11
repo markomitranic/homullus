75f4276eccc026296cb6218b779318e3c0bfb0b0
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-04-11 15:52:30 +0200

    Update docker nginx conf

--- Docker/deploy_prod.sh
+++ Docker/deploy_prod.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-sudo cp /etc/letsencrypt/live/homullus.com/fullchain.pem nginx/ssl/
-sudo cp /etc/letsencrypt/live/homullus.com/privkey.pem nginx/ssl/
-sudo cp /etc/letsencrypt/options-ssl-nginx.conf nginx/ssl/
-sudo cp /etc/letsencrypt/ssl-dhparams.pem nginx/ssl/
-
-docker-compose build && docker-compose up -d
+docker-compose down && docker-compose build && docker-compose up -d
